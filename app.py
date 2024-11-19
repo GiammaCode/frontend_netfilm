648d55f14ec8cc93e9f627bc6aa8c77e6c54de88
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-11-19 19:34:24 +0100

    add documentation code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# URL dei microservizi
+# URLs of the microservices
 AUTHENTICATION_SERVICE_URL = "http://localhost:5000"
 CONTENT_MANAGEMENT_SERVICE_URL = "http://localhost:5001"
 
@@ -14,14 +14,20 @@ CORS(app)
 # Homepage
 @app.route('/')
 def index():
+    """
+    Render the index page.
+    """
     return render_template('index.html')
 
-# Pagina principale
+# Home page after login
 @app.route('/home')
 def home():
+    """
+    Render the home page with a list of contents fetched from the content management service.
+    Requires a valid token passed in the query string.
+    """
     token = request.args.get('token')
     headers = {'Authorization': f'Bearer {token}'}
-    # Chiamata al servizio di gestione dei contenuti per ottenere la home
     response = requests.get(f"{CONTENT_MANAGEMENT_SERVICE_URL}/content", headers=headers)
     if response.status_code == 200:
         contents = response.json()
@@ -29,16 +35,18 @@ def home():
     else:
         return "Unable to fetch content", response.status_code
 
-###########################  AUTH #############################
-
-# Pagina di login
+#Start AUTH service
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Render the login page and handle the login process.
+    Send a request to the authentication service to log in the user.
+    If successful, redirects to the home page with the token as query parameter.
+    """
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        # Invio della richiesta POST all'auth service per il login
         response = requests.post(f"{AUTHENTICATION_SERVICE_URL}/auth/login", json={'email': email, 'password': password})
         if response.status_code == 200:
             token = response.json().get('access_token')
@@ -48,14 +56,17 @@ def login():
 
     return render_template('login.html')
 
-# Pagina di registrazione ok
+# Registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    Render the registration page and handle user registration.
+    Send a request to the authentication service to register the user.
+    If successful, redirects to the home page with the token as query parameter.
+    """
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        # Invio della richiesta POST all'auth service per la registrazione
         response = requests.post(f"{AUTHENTICATION_SERVICE_URL}/auth/register", json={'email': email, 'password': password})
         if response.status_code == 201:
             token = response.json().get('access_token')
@@ -65,53 +76,57 @@ def register():
 
     return render_template('register.html')
 
-#pagina di reset
+# Reset password Page
 @app.route('/reset-password', methods=['GET', 'POST'])
 def reset_password():
+    """
+    Render the reset password page and handle the password reset process.
+    Send a request to the authentication service to initiate a password reset for the user.
+    """
     if request.method == 'POST':
         email = request.form.get('email')
-
         if not email:
             return "Please provide a valid email.", 400
-
-        # Invio della richiesta POST all'auth service per il reset della password
         response = requests.post(f"{AUTHENTICATION_SERVICE_URL}/auth/reset-password", json={'email': email})
-        
         if response.status_code == 200:
             return "Password reset email sent. Please check your inbox.", 200
         elif response.status_code == 404:
             return "User not found.", 404
         else:
             return "An error occurred.", response.status_code
-
+        
     return render_template('reset_password.html')
 
 
-# Logout dell'utente
+# Logout user
 @app.route('/logout', methods=['GET'])
 def logout():
+    """
+    Handle the logout process by removing the token from the session.
+    Send a request to the authentication service to log out the user.
+    """
     token = session.get('access_token')
     if token:
         headers = {'Authorization': f'Bearer {token}'}
-        # Invio della richiesta di logout al servizio di autenticazione
         response = requests.post(f"{AUTHENTICATION_SERVICE_URL}/auth/logout", headers=headers)
         if response.status_code == 200:
-            session.pop('access_token', None)  # Rimuovi il token dalla sessione
+            session.pop('access_token', None)  #Remove token
             return redirect(url_for('index'))
         else:
             return "Logout failed", response.status_code
     return redirect(url_for('index'))
 
 
-##############################  CONTENT MANAGMENT #############################
-
-# Pagina per visualizzare dettagli di un contenuto specifico
+#Start CONTENT_MANAGMENT service
+# Page to view content details
 @app.route('/content/<int:content_id>', methods=['GET'])
 def content_detail(content_id):
+    """
+    Render the page to show details of a specific content.
+    Send a request to the content management service to fetch the content details by ID.
+    """
     token = request.args.get('token')
     headers = {'Authorization': f'Bearer {token}'}
-    
-    # Chiamata al servizio di gestione dei contenuti per ottenere i dettagli
     response = requests.get(f"{CONTENT_MANAGEMENT_SERVICE_URL}/content/{content_id}", headers=headers)
     if response.status_code == 200:
         content = response.json()
@@ -120,44 +135,47 @@ def content_detail(content_id):
         return "Content not found", response.status_code
         
 
-# Pagina per visualizzare dettagli di un contenuto specifico
+# # Page to view all contents
 @app.route('/content', methods=['GET'])
 def content():
+    """
+    Render the page to view all available content.
+    Send a request to the content management service to fetch all content.
+    """
     token = request.args.get('token')
     headers = {'Authorization': f'Bearer {token}'}
-    # Chiamata al servizio di gestione dei contenuti per ottenere i dettagli
+    # Chiamata al servizio di gestione dei contenuti per ottenere la home
     response = requests.get(f"{CONTENT_MANAGEMENT_SERVICE_URL}/content", headers=headers)
     if response.status_code == 200:
-        content = response.json()
-        return render_template('all_content.html', content=content, token=token)
-    else:
-        return "Content not found", response.status_code
+        contents = response.json()
+        return render_template('all_content.html', contents=contents, token=token)
 
+# Stream content page
 @app.route('/content/<int:content_id>/stream', methods=['GET'])
 def stream_content(content_id):
-    # Effettua una richiesta al servizio di gestione dei contenuti
+    """
+    Render the page to stream a specific content.
+    Send a request to the content management service to get streaming information for the content by ID.
+    """
     response = requests.get(f"{CONTENT_MANAGEMENT_SERVICE_URL}/content/{content_id}/stream")
-    
     if response.status_code == 200:
-        # Se la risposta ha successo, ottieni i dettagli del contenuto per visualizzare la pagina di streaming
         content = response.json().get('content')
         return render_template('stream.html', content=content)
     else:
-        # In caso di errore, restituisce il messaggio di errore e il codice di stato appropriato
         return "Unable to stream content", response.status_code
 
-# Pagina per il download del contenuto
+# Download content page
 @app.route('/content/<int:content_id>/download', methods=['GET'])
 def download_content(content_id):
-     # Effettua una richiesta al servizio di gestione dei contenuti
+    """
+    Render the page to download a specific content.
+    Send a request to the content management service to get download information for the content by ID.
+    """
     response = requests.get(f"{CONTENT_MANAGEMENT_SERVICE_URL}/content/{content_id}/stream")
-    
     if response.status_code == 200:
-        # Se la risposta ha successo, ottieni i dettagli del contenuto per visualizzare la pagina di streaming
         content = response.json().get('content')
         return render_template('download.html', content=content)
     else:
-        # In caso di errore, restituisce il messaggio di errore e il codice di stato appropriato
         return "Unable to stream content", response.status_code
 
 if __name__ == '__main__':
